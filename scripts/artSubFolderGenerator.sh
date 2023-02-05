70b028692f466bb4cb64600545f5bb08c7089c48
Joseph Klaszky <jfklaszky@gmail.com>
2023-02-05 10:27:17 -0500

    Fixing bad var name

--- scripts/artSubFolderGenerator.sh
+++ scripts/artSubFolderGenerator.sh
@@ -6,8 +6,8 @@ do
 	base_file=$(basename "$f")
 	base_file_split=($base_file)
 	art_num=${base_file_split[0]}
-	art_dir="$file_path/$art_num Art"
-	sep_dir="$file_path/$art_num Sep"
+	art_dir="$f/$art_num Art"
+	sep_dir="$f/$art_num Sep"
 
 	mkdir "$art_dir"
 	mkdir "$sep_dir"
